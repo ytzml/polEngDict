38b486a9a8ede89dc9796056cfbd312809e03421
Jacek Malyszko <jacek.malyszko@gmail.com>
2020-11-11 15:53:06 +0100

    Run with gunicorn

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ sudo apt update
 sudo apt install -y git
 sudo apt install -y python3-pip
 sudo apt install -y screen
+sudo apt install -y gunicron
 git clone https://github.com/ytzml/polEngDict.git
-screen -S polEngDictDeploy
 cd polEngDict
 pip3 install -r requirements.txt
-python3 app.py
+gunicorn app:app -b 0.0.0.0:8080 --daemon
